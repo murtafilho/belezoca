5eb1d3aed45dcef8183c2cf643e9915b9ed79cde
murtafilho <murtafilho@gmail.com>
2020-10-12 18:41:29 -0300

    c4

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -96,15 +96,6 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/admin-lte/2.4.3/js/adminlte.min.js"></script>
 
 <script src="https://cdnjs.cloudflare.com/ajax/libs/iCheck/1.0.2/icheck.min.js"></script>
-<script>
-    $(function () {
-        
-        $('input').iCheck({
-            checkboxClass: 'icheckbox_square-blue',
-            radioClass: 'iradio_square-blue',
-            increaseArea: '20%' // optional
-        });
-    });
-</script>
+
 </body>
 </html>
